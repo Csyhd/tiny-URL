9df3e6bf9b808f67cde3dc83e10c626535c5db02
Csyhd <csyhd@msn.com>
2020-12-11 21:31:48 +0800

    refactor:refactor index.js and guide.js

--- routes/modules/guide.js
+++ routes/modules/guide.js
@@ -4,13 +4,13 @@ const Urls = require('../../models/Urls')
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
+  console.log(id)
   Urls.findOne({ tinyURL: id })
     .then((url) => {
       const URL = url.URL
-      res.location(URL)
+      res.redirect(URL)
     })
-
+    .catch(error => console.log(error))
 })
-
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,14 +6,5 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id
-  Urls.findOne({ tinyURL: id })
-    .then((url) => {
-      const URL = url.URL
-      res.location(URL)
-    })
-
-})
 
 module.exports = router
