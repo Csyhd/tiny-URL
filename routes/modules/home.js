45b914f16b406b5f5ec4da52a45b62d9051bcdc8
Csyhd <csyhd@msn.com>
2020-12-11 21:08:17 +0800

    fix:Urls is not define

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-// const Url = require('../../models/Urls')
+const Urls = require('../../models/Urls')
 
 router.get('/', (req, res) => {
   res.render('index')
