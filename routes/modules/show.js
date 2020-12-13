a2b00734e7135b0805ba820310d0e307c1b777dc
Csyhd <csyhd@msn.com>
2020-12-13 11:00:58 +0800

    feat:update layout and add copy function

--- routes/modules/show.js
+++ routes/modules/show.js
@@ -5,31 +5,36 @@ const creatRandomNumber = require('../../public/javacsripts/transform')
 
 
 router.post('/', (req, res) => {
-  let URL = req.body.URL
-  let tinyURL = ''
-  let creatURL = ''
-  let status = true
 
-  Urls.find()
-    .then((urls) => {
-      while (status) { //使用while迴圈狀態判斷短網址是否重複
-        tinyURL = creatRandomNumber() //呼叫亂數產生函數
-        status = urls.some((url) => { //比對亂數是否重複並且回傳狀態
-          return url.randomNumbers === tinyURL
-        })
-      }
-    })
-    .then(() => {
-      creatURL = {
-        URL,
-        tinyURL
-      }
-      return Urls.create(creatURL)// 把產生的資料丟進資料庫裡
-        .then(() => {
-          res.render('show', { creatURL })//render畫面
-        })
-    })
-    .catch(error => console.log(error))
+  let URL = req.body.URL
+  if (URL !== '') {
+    let tinyURL = ''
+    let creatURL = ''
+    let status = true
+
+    Urls.find()
+      .then((urls) => {
+        while (status) { //使用while迴圈狀態判斷短網址是否重複
+          tinyURL = creatRandomNumber() //呼叫亂數產生函數
+          status = urls.some((url) => { //比對亂數是否重複並且回傳狀態
+            return url.randomNumbers === tinyURL
+          })
+        }
+      })
+      .then(() => {
+        creatURL = {
+          URL,
+          tinyURL
+        }
+        return Urls.create(creatURL)// 把產生的資料丟進資料庫裡
+          .then(() => {
+            res.render('show', { creatURL })//render畫面
+          })
+      })
+      .catch(error => console.log(error))
+  } else {
+    res.redirect('/')
+  }
 })
 
 module.exports = router
